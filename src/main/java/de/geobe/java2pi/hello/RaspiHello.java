00c9dcd1fad78ebe859b441c20d3b7a3ca679958
geobe <code@georg.beier.de>
2020-04-17 16:41:41 +0200

    First working version

--- src/main/java/de/geobe/java2pi/hello/RaspiHello.java
+++ src/main/java/de/geobe/java2pi/hello/RaspiHello.java
@@ -1,9 +1,31 @@
-package de.geobe.java2pi.hello;
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2020.  Georg Beier. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
-import java.util.Properties;
+package de.geobe.java2pi.hello;
 
 public class RaspiHello {
     public static void main(String[] args) {
-        System.out.printf("Hello from %s on %s\n", System.getProperty("os.name"), System.getProperty("os.arch"));
+        System.out.printf("Great HELLO from %s on %s\n", System.getProperty("os.name"), System.getProperty("os.arch"));
     }
 }
